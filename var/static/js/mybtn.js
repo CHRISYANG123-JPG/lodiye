4d50d54eadf72b04790af1ed389e688223254384
YANG_JIA_SHUN <57613811+CHRISYANG123-JPG@users.noreply.github.com>
2025-04-10 20:28:14 +0800

    Add files via upload

---
+++ var/static/js/mybtn.js
@@ -0,0 +1,11 @@
+function goToRandomWhatsApp() {
+    const links = [
+      "https://t.me/Anita95219",
+      "https://t.me/Adriana1437",
+      "https://t.me/Maria14561",
+      "https://t.me/camila258369" // 替换为你的第一个号
+      
+    ];
+    const randomIndex = Math.floor(Math.random() * links.length);
+    window.location.href = links[randomIndex];
+  }
